0b5095c2d64cc68056fd442be6c2caf8283ef703
nkhoroshavin <74411205+nkhoroshavin@users.noreply.github.com>
2021-05-24 01:14:20 +0500

    Add files via upload

---
+++ PostfixCalculator/main.cpp
@@ -0,0 +1,11 @@
+#include "postfixcalc.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    PostfixCalc w;
+    w.show();
+    return a.exec();
+}
